d91603a593d905f5b4a65606f836a5e08e91a2bb
freakymind12 <riankurniawan132@gmail.com>
2025-05-19 09:58:05 +0700

    add parent booking page

---
+++ src/views/booking/TheBooking.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-row :gutter="[8, 8]" :wrap="true">
+    <a-col :sm="24" :lg="12" :xl="12">
+      <a-row>
+        <BookingSchedule />
+      </a-row>
+    </a-col>
+    <a-col :sm="24" :lg="12" :xl="12">
+      <a-row>
+        <UserBookingList />
+      </a-row>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup>
+import BookingSchedule from './components/BookingSchedule.vue'
+import UserBookingList from './components/UserBookingList.vue'
+</script>
