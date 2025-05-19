0c9023b41897463949a76a278888f146ee355fa4
freakymind12 <riankurniawan132@gmail.com>
2025-05-19 10:10:09 +0700

    add report rooms component

---
+++ src/views/dashboard/components/ReportRooms.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-card size="small" :hoverable="true" class="report-card">
+    <span class="large bold">Graphic Bookings By Rooms</span>
+    <BarChart
+      height="545px"
+      title="Booking"
+      :x-axis-data="reportStore.formattedRoomSummary[0]"
+      :y-axis-data="reportStore.formattedRoomSummary[1]"
+    />
+  </a-card>
+</template>
+
+<script setup>
+import BarChart from '@/components/BarChart.vue'
+import { useReportStore } from '@/stores/report'
+const reportStore = useReportStore()
+</script>
