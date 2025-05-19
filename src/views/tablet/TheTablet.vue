5f2686fd3f5799a198b711493eee0fc92e0bf44f
freakymind12 <riankurniawan132@gmail.com>
2025-05-19 10:12:19 +0700

    add parent tablet view page

---
+++ src/views/tablet/TheTablet.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-flex justify="center" gap="small" align="center" class="tablet-view">
+    <RoomInfo />
+    <RoomSchedule />
+  </a-flex>
+</template>
+
+<script setup>
+import RoomInfo from '@/views/tablet/component/RoomInfo.vue'
+import RoomSchedule from '@/views/tablet/component/RoomSchedule.vue'
+import { onMounted } from 'vue'
+import { useBookingStore } from '@/stores/bookings'
+import { useRoute } from 'vue-router'
+import dayjs from 'dayjs'
+
+const bookingStore = useBookingStore()
+
+onMounted(async () => {
+  await bookingStore.get({
+    id_room: useRoute().params.id_room,
+    date: dayjs().format('YYYY-MM-DD'),
+    status: 'All',
+  })
+})
+</script>
+
+<style scoped>
+.tablet-view {
+  height: 100dvh;
+  background: #f0f2f5;
+}
+</style>
